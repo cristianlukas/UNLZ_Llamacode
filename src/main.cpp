b79f2fb743c291fb422f4ccbf7bb9c64abd76276
guideahon <45019062+guideahon@users.noreply.github.com>
2026-07-06 20:47:06 -0300

    chore: bump version 0.1.5 -> 0.1.6 (auto en build)

--- src/main.cpp
+++ src/main.cpp
@@ -111,7 +111,7 @@ int main(int argc, char *argv[])
     app.setWindowIcon(appIcon);
     app.setApplicationName("LlamaCode");
     app.setOrganizationName("LlamaCode");
-    app.setApplicationVersion("0.1.5");
+    app.setApplicationVersion("0.1.6");
 
     qDebug() << "QApplication ready";
 
