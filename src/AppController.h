7246e62f12a8246cc32743559a9a7d46d06bb45f
guideahon <45019062+guideahon@users.noreply.github.com>
2026-07-06 19:26:36 -0300

    chore: bump version 0.1.4 -> 0.1.5 (auto en build)

--- src/AppController.h
+++ src/AppController.h
@@ -377,7 +377,7 @@ public:
     Q_INVOKABLE void smokeTestServer(const QString &launchProfileId);
     Q_INVOKABLE bool smokeTestRunning() const { return m_smokeTestProc != nullptr; }
     Q_INVOKABLE QString resolveFlag(const QString &binaryId, const QString &flag) const;
-    Q_INVOKABLE QString version() const { return QStringLiteral("0.1.4"); }
+    Q_INVOKABLE QString version() const { return QStringLiteral("0.1.5"); }
     bool updateAvailable() const { return m_updateAvailable; }
     QVariantMap updateInfo() const { return m_updateInfo; }
     Q_INVOKABLE void checkForUpdates();
